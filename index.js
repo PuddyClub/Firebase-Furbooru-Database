b0190d4c6d455630d7932d80866995418ca68fe1
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-16 18:12:28 -0300

    First Commit based in the Derpibooru Repository

---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = function (data) {
+
+    // Exist Data
+    if (data) {
+
+        // Prepare Generator
+        const philomena_generator = require('@tinypudding/firebase-philomena-database');
+
+        // Send the Derpibooru Module
+        return philomena_generator({
+
+            // Derpibooru Settings
+            booru: {
+                version: 1,
+                id: 'derpibooru',
+                name: 'Derpibooru',
+                url: 'https://derpibooru.org',
+                tagListVar: 'tags',
+                idVar: 'id',
+            },
+
+            // Config
+            config: data.config,
+
+            // Module
+            module: data.module
+
+        });
+
+    }
+
+    // Nope
+    else { throw new Error('The data value is empty!'); }
+
+};
