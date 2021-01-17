1515c51158554de81582a1f99c70a15a2e9ccfd4
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-17 10:39:34 -0300

    filter id

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = function (data) {
 
             // Derpibooru Settings
             booru: {
+                filter_id: 2,
                 version: 1,
                 id: 'furbooru',
                 name: 'Furbooru',
